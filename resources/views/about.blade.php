593bfadd8da370e32db26111e187e70185bc0ce2
sShi1ro <muhammad.awparif.hakimi@gmail.com>
2025-08-15 11:55:29 +0800

    about us view & controller

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Us</title>
+    </head>
+    <body>
+        <h1>About Our Team</h1>
+        <h2>{{ $name }}</h2>
+        <p>We are a dedicated team of developers working on the SKSU Room Booking system.</p>
+    </body>
+</html>
